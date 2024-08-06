0c01ac3bede48e100b517addad85241c51a671fa
Kylee Sawyer <k.sawyer@ufl.edu>
2024-08-06 02:42:33 -0400

    updating file names

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require("./user");
+const User = require("./User");
 const StudySet = require('./StudySet')
-const Flashcard = require("./flashcard")
+const Flashcard = require("./Flashcard")
 
 module.exports = { User, StudySet, Flashcard };
 
